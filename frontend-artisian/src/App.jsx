2e0d59c6e63924b3fe8f8a2809d339753165b09a
Ompawaskar <138382841+Ompawaskar@users.noreply.github.com>
2024-10-05 15:58:04 +0530

    Add Admin Dashboard,Orders,Inventory

--- frontend-artisian/src/App.jsx
+++ frontend-artisian/src/App.jsx
@@ -7,7 +7,7 @@ import Wishlist from "./pages/Wishlist";
 import Profile from "./pages/Profile";
 import Cart from "./pages/Cart";
 import SearchResult from "./pages/SearchResult";
-import DashBoard from "./pages/Dashboard";
+import { Dashboard } from "./components/Dashboard-Admin/DashBoard";
 import Community from "./pages/Community";
 import Education from "./pages/Education";
 import AuthPage from "./pages/AuthPage";
@@ -19,6 +19,8 @@ import CallPage from "./pages/CallPage";
 import { TodoProvider } from "./context/TodoContext";
 import { AppointmentProvider } from "./context/AppointmentContext";
 import { MedicationProvider } from "./context/MedicationContext";
+import { Inventory } from "./components/Inventory/Inventory";
+import { Orders } from "./components/Orders/Order";
 
 
 
@@ -30,13 +32,16 @@ function App() {
 
       <Route path="/cart" element={<Cart />}></Route>
       <Route path="/searchresult" element={<SearchResult />}></Route>
-      <Route path="/dashboard" element={<MedicationProvider><AppointmentProvider><TodoProvider><DashBoard /></TodoProvider></AppointmentProvider></MedicationProvider>}></Route>
+      {/* <Route path="/dashboard" element={<MedicationProvider><AppointmentProvider><TodoProvider><DashBoard /></TodoProvider></AppointmentProvider></MedicationProvider>}></Route> */}
       <Route path="/community" element={<Community />}></Route>
       
       <Route path="/medical" element={<MedicalHistoryPage />}></Route>
       <Route path="/caregiver" element={<Caregiver />}></Route>
       <Route path="/doctor" element={<Doctor />}></Route>
       {/* <Route path="/connect" element={<CallPage />}></Route> */}
+      <Route path="/dashboard" element = {<Dashboard />} />
+      <Route path="/inventory" element = {<Inventory />} />
+      <Route path='/orders' element={<Orders />} />
     </Routes>
   );
 }
---
+++ frontend-artisian/src/components/Dashboard-Admin/BarChart.jsx
@@ -0,0 +1,91 @@
+import { TrendingUp } from "lucide-react"
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A stacked bar chart with a legend"
+
+const chartData = [
+  { type: "Pottery", high: 500, low: 250 },
+  { type: "Woven Baskets", high: 400, low: 150 },
+  { type: "Leather Goods", high: 350, low: 200 },
+  { type: "Sculptures", high: 300, low: 100 },
+  { type: "Textile Products", high: 450, low: 220 },
+  { type: "Jewelry", high: 550, low: 300 }
+];
+
+  
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+} 
+
+export function BarChartComponent() {
+  return (
+    <Card className="shadow-lg border dark:border-gray-800 dark:shadow-lg">
+      <CardHeader>
+        <CardTitle>Sales Categorized by type's</CardTitle>
+        <CardDescription>September 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+            className="text-black"
+              dataKey="type"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 100)}
+            />
+            <ChartTooltip content={<ChartTooltipContent hideLabel className = "bg-orange-800 text-orange-800" />}  />
+            <ChartLegend content={<ChartLegendContent className = "text-orange-800"   />} />
+            <Bar
+              dataKey="high"
+              stackId="a"
+              fill="rgb(154, 52, 18)"
+              radius={[0, 0, 4, 4]}
+              
+            />
+            <Bar
+              dataKey="low"
+              stackId="a"
+              fill="rgb(181, 101, 29)"
+              radius={[4, 4, 0, 0]}
+            />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 font-medium leading-none text-green-600">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4 text-red-600" />
+        </div>
+        <div className="leading-none text-muted-foreground ">
+          Showing total sales for last month
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
